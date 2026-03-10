22756a3f6643f9a5b043ec1a93e4ae099135390f
Abigail Balogun <abalogun333@gmail.com>
2026-03-10 04:23:27 -0700

    Movie app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./css/App.css";
+import Home from "./pages/home";
+import Favorite from "./pages/favorites";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <MovieProvider>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </MovieProvider>
+  );
+}
+
+export default App;
